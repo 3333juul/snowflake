3416f043a94119cca21bb589bf400fa74e62747d
luravoid <example@example.com>
2024-10-25 19:00:35 +0200

    edit bluetooth

--- modules/core/bluetooth.nix
+++ modules/core/bluetooth.nix
@@ -3,8 +3,21 @@
   pkgs,
   ...
 }: {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        FastConnectable = true;
+      };
+      Policy = {
+        ReconnectAttempts = 7;
+        ReconnectIntervals = "1,2,3,4,8";
+        AutoEnable = true;
+      };
+    };
+    powerOnBoot = true;
+  };
+
   services.blueman.enable = true;
 
   environment.systemPackages = with pkgs; [
