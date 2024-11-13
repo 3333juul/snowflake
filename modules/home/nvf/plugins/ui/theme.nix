60b27bedc5637cf6f244701dceeb90c5753212d1
luravoid <example@example.com>
2024-11-13 03:32:19 +0100

    nvf - change theme

--- modules/home/nvf/plugins/ui/theme.nix
+++ modules/home/nvf/plugins/ui/theme.nix
@@ -1,33 +1,33 @@
 {pkgs, ...}: {
   programs.nvf.settings.vim = {
     theme = {
-      enable = false;
+      enable = true;
       name = "gruvbox";
       style = "dark";
       transparent = true;
     };
-    extraPlugins = with pkgs.vimPlugins; {
-      gruvbox-material-nvim = {
-        package = gruvbox-material-nvim;
-        setup = "require('gruvbox-material').setup({
-          italics = true,         
-          contrast = 'medium',        
-          comments = {
-            italics = true,          
-          },
-          background = {
-            transparent = true,    
-          },
-          float = {
-            force_background = false,
-            background_color = nil,                                  
-          },
-          signs = {
-            highlight = true,         
-          },
-          customize = nil,                                           
-        })";
-      };
-    };
+    # extraPlugins = with pkgs.vimPlugins; {
+    #   gruvbox-material-nvim = {
+    #     package = gruvbox-material-nvim;
+    #     setup = "require('gruvbox-material').setup({
+    #       italics = true,
+    #       contrast = 'medium',
+    #       comments = {
+    #         italics = true,
+    #       },
+    #       background = {
+    #         transparent = true,
+    #       },
+    #       float = {
+    #         force_background = false,
+    #         background_color = nil,
+    #       },
+    #       signs = {
+    #         highlight = true,
+    #       },
+    #       customize = nil,
+    #     })";
+    #   };
+    # };
   };
 }
