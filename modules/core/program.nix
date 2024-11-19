c6f0e8ad01a548e5388e2ed6f32444f09dae13d5
luravoid <example@example.com>
2024-11-19 09:04:46 +0100

    edit program.nix

--- modules/core/program.nix
+++ modules/core/program.nix
@@ -3,13 +3,17 @@
   lib,
   ...
 }: {
-  programs.dconf.enable = true;
-  programs.zsh.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-    # pinentryFlavor = "";
+  programs = {
+    dconf.enable = true;
+    zsh.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+      # pinentryFlavor = "";
+    };
+    nix-ld = {
+      enable = true;
+      libraries = with pkgs; [];
+    };
   };
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [];
 }
