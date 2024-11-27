8e29b12b6f404a1ed5a2b108e5aeaf05e42e3b6f
luravoid <example@example.com>
2024-11-27 22:43:40 +0100

    edit vocabsieve derivation

--- pkgs/vocabsieve.nix
+++ pkgs/vocabsieve.nix
@@ -1,85 +1,82 @@
 {
   lib,
   pkgs,
-  ...
-}:
-pkgs.python311Packages.buildPythonPackage rec {
-  pname = "vocabsieve";
-  version = "0.12.2";
-  src = pkgs.fetchPypi {
-    inherit pname version;
-    sha256 = "1440e1682b0ba57312abf552cf7ee821a5013901887108ddbd7a58a3f7774c94";
-  };
-  format = "wheel";
-  nativeBuildInputs = [
-    pkgs.python311Packages.setuptools
-    pkgs.python311Packages.wheel
-    pkgs.python311Packages.build
-    pkgs.python311Packages.installer
-    pkgs.libsForQt5.qt5.wrapQtAppsHook
-  ];
-  buildInputs = [
-    pkgs.qt5.qtbase
-    pkgs.qt5.qtwayland
-  ];
-  propagatedBuildInputs = with pkgs.python311Packages; [
-    beautifulsoup4
-    bidict
-    charset-normalizer
-    ebooklib
-    flask
-    loguru
-    lxml
-    markdown
-    markdownify
-    mobi
-    packaging
-    pymorphy3
-    pymorphy3-dicts-ru
-    pymorphy3-dicts-uk
-    pynput
-    pyqt5-multimedia
-    pyqtdarktheme
-    pyqtgraph
-    pystardict
-    pysubs2
-    python-lzo
-    readmdict
-    requests
-    sentence-splitter
-    simplemma
-    slpp
-    typing-extensions
-    waitress
-  ];
-  dontWrapQtApps = true;
-  preFixup = ''
-    makeWrapperArgs+=("''${qtWrapperArgs[@]}")
-  '';
-  doCheck = false;
-  unpackPhase = ''
-    tar -xzf ${src} -C $TMPDIR
-  '';
-  buildPhase = ''
-    cd $TMPDIR/vocabsieve-0.12.2
-    python3 -m build --wheel --no-isolation --outdir=dist
-  '';
-  installPhase = ''
-    runHook preInstall
-    cd $TMPDIR/vocabsieve-0.12.2
-    python3 -m installer dist/*.whl --prefix=$out
-    runHook postInstall
-  '';
-  postInstall = ''
-    mkdir -p $out/share/applications
-    cp $TMPDIR/vocabsieve-0.12.2/vocabsieve.desktop $out/share/applications
-    mkdir -p $out/share/icons
-    cp $TMPDIR/vocabsieve-0.12.2/vocabsieve.png $out/share/icons
-  '';
-  meta = with lib; {
-    description = "A simple, effective sentence mining tool";
-    homepage = "https://github.com/FreeLanguageTools/vocabsieve";
-    license = licenses.gpl3Plus;
-    maintainers = [];
-  };
-}
+  stdenv,
+  python3,
+  fetchFromGitHub,
+  python3Packages,
+  gst_all_1,
+}: let
+  pyqtdarktheme-fork = python3Packages.pyqtdarktheme.overridePythonAttrs (oldAttrs: {
+    pname = "pyqtdarktheme";
+    version = "2.3.2";
+    src = fetchFromGitHub {
+      owner = "henriquegemignani";
+      repo = "PyQtDarkTheme";
+      rev = "4715e906f005bf70f19bc4f1f4dc4cf7d08c175a";
+      sha256 = "sha256-3pdwqhrJiZk/NM7XoE4EGhd7wUCWiXBu9/Nc/tekqcI=";
+    };
+  });
+in
+  python3.pkgs.buildPythonApplication rec {
+    pname = "vocabsieve";
+    version = "0.12.4";
+    format = "pyproject";
+    src = fetchFromGitHub {
+      owner = "FreeLanguageTools";
+      repo = pname;
+      rev = "v${version}";
+      hash = "sha256-LTu/3I4blP72CaZo6o3fTDoufK9QNtSjE5umfvEpwAE=";
+    };
+
+    nativeBuildInputs = [
+      pkgs.qt5.wrapQtAppsHook
+      gst_all_1.gst-libav
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-vaapi
+      gst_all_1.gstreamer
+    ];
+
+    preFixup = ''
+      makeWrapperArgs+=("''${qtWrapperArgs[@]}")
+      makeWrapperArgs+=(--prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0")
+    '';
+
+    buildInputs = [pkgs.qt5.qtbase pkgs.qt5.qtmultimedia python3Packages.setuptools];
+
+    propagatedBuildInputs = with python3Packages; [
+      bidict
+      ebooklib
+      flask
+      flask-sqlalchemy
+      lxml
+      markdown
+      markdownify
+      mobi
+      packaging
+      pynput
+      pymorphy3
+      pymorphy3-dicts-ru
+      pymorphy3-dicts-uk
+      pyqt5-multimedia
+      pyqtgraph
+      pyqtdarktheme-fork
+      pysubs2
+      pystardict
+      python-lzo
+      readmdict
+      requests
+      sentence-splitter
+      simplemma
+      slpp
+      waitress
+    ];
+
+    meta = with lib; {
+      description = "Simple sentence mining tool for language learning";
+      homepage = "https://github.com/FreeLanguageTools/vocabsieve";
+      license = licenses.gpl3;
+      maintainers = [maintainers.rasmusrendal];
+    };
+  }
