2ea29158e38a3697a72f4a4350cc577f0340ebac
luravoid <example@example.com>
2024-12-13 03:48:12 +0100

    edit rofi

--- modules/home/rofi/rofi.nix
+++ modules/home/rofi/rofi.nix
@@ -3,33 +3,28 @@
   config,
   ...
 }: {
-  #home.packages = with pkgs; [rofi-wayland];
-
   programs.rofi = {
     enable = true;
     package = pkgs.callPackage ../../../pkgs/rofi.nix {};
+    extraConfig = {
+      show-icons = true;
+      icon-theme = "Zafiro";
+      terminal = "kitty";
+      cycle = true;
+      hide-scrollbar = true;
+      disable-history = false;
+      kb-row-up = "Up,Control+k,Shift+Tab,Shift+ISO_Left_Tab";
+      kb-row-down = "Down,Control+j";
+      kb-row-left = "Control+h";
+      kb-row-right = "Control+l";
+      kb-accept-entry = "Control+m,Return,KP_Enter";
+      kb-remove-to-eol = "Control+Shift+e";
+      kb-mode-next = "Shift+Right,Control+Tab";
+      kb-mode-previous = "Shift+Left,Control+Shift+Tab";
+      kb-remove-char-back = "BackSpace";
+      kb-mode-complete = "Control+t";
+    };
   };
-
-  xdg.configFile."rofi/config.rasi".text = ''
-    configuration {
-      show-icons:         true;
-      icon-theme:         "Zafiro";
-      terminal:           "kitty";
-      cycle:              true;
-      hide-scrollbar:     true;
-      disable-history:    false;
-      kb-row-up: "Up,Control+k,Shift+Tab,Shift+ISO_Left_Tab";
-      kb-row-down: "Down,Control+j";
-      kb-row-left: "Control+h";
-      kb-row-right: "Control+l";
-      kb-accept-entry: "Control+m,Return,KP_Enter";
-      kb-remove-to-eol: "Control+Shift+e";
-      kb-mode-next: "Shift+Right,Control+Tab";
-      kb-mode-previous: "Shift+Left,Control+Shift+Tab";
-      kb-remove-char-back: "BackSpace";
-      kb-mode-complete: "Control+t";
-    }
-  '';
 
   # Symlink all themes - the argument of this function must be a full path, not a Nix path type, otherwise it won't work
   xdg.configFile."rofi/themes" = {
