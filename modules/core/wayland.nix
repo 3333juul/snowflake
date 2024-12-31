be454be4bf485e1b1c9b009ef4ea71d1a1585719
luravoid <example@example.com>
2024-12-31 16:19:07 +0100

    edit hyprland nixos module

--- modules/core/wayland.nix
+++ modules/core/wayland.nix
@@ -1,12 +1,21 @@
-{ inputs, pkgs, ... }:
 {
-  programs.hyprland.enable = true;
+  inputs,
+  pkgs,
+  ...
+}: {
+  programs.hyprland = {
+    enable = true;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
   xdg.portal = {
     enable = true;
     wlr.enable = true;
     xdgOpenUsePortal = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
+      #pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal-gtk
     ];
   };
