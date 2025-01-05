df180cedff4e968cd9ffcc0c3f81c2494aea7f3c
luravoid <example@example.com>
2025-01-05 10:06:40 +0100

    mpv - add bindings

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -3,6 +3,14 @@
     enable = true;
     package = pkgs.mpv;
     bindings = {
+      h = "seek -5";
+      l = "seek 5";
+      j = "cycle sub";
+      J = "cycle sub down";
+      k = "cycle audio";
+      K = "cycle audio down";
+      H = "seek -60";
+      L = "seek 60";
     };
     config = {
       keep-open = "yes";
