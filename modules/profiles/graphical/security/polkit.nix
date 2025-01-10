eaa111edd0a1b84dd7396d859c43a7a86cc17443
luravoid <example@example.com>
2025-01-10 23:35:10 +0100

    polkit - add mkIf isWM

--- modules/profiles/graphical/security/polkit.nix
+++ modules/profiles/graphical/security/polkit.nix
@@ -1,15 +1,27 @@
 {
   lib,
   pkgs,
+  config,
   ...
-}: {
-  systemd.user.services.polkit-authentication-agent = {
-    description = "PolicyKit Authentication Agent";
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
-      #ExecStart = "${inputs.hyprpolkitagent.packages.${pkgs.system}.hyprpolkitagent}/libexec/hyprpolkitagent";
+}: let
+  inherit (lib.modules) mkIf;
+
+  cfg = config.garden.environment;
+in {
+  systemd.user.services = mkIf cfg.isWM {
+    polkit-authentication-agent = {
+      description = "PolicyKit Authentication Agent";
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
+        #ExecStart = "${inputs.hyprpolkitagent.packages.${pkgs.system}.hyprpolkitagent}/libexec/hyprpolkitagent";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
     };
-    wantedBy = ["graphical-session.target"];
   };
 }
