fa803f913074b7d5858f2a1c1b650463420f5b9c
luravoid <example@example.com>
2025-01-12 14:22:17 +0100

    add ttrss

---
+++ modules/profiles/server/services/ttrss.nix
@@ -0,0 +1,20 @@
+{
+  services.tt-rss = {
+    enable = false;
+    selfUrlPath = "http://localhost";
+
+    #themePackages = [pkgs.tt-rss-theme-feedly];
+    #pluginPackages = [pkgs.tt-rss-plugin-auth-ldap];
+    # plugins = [
+    #   "auth_internal"
+    #   "auth_ldap"
+    #   "note"
+    #   "updater"
+    #   "api_feedreader"
+    # ];
+
+    database = {
+      type = "pgsql";
+    };
+  };
+}
