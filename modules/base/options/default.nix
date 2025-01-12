24ebbc554e80ee05b35dd98e1d88a5e6ac2533e0
luravoid <example@example.com>
2025-01-13 00:14:42 +0100

    edit options

--- modules/base/options/default.nix
+++ modules/base/options/default.nix
@@ -1,14 +1,11 @@
 {...}: {
   imports = [
+    ./programs
     ./style
-    ./cli.nix
     ./device.nix
     ./environment.nix
-    ./gaming.nix
-    ./gui.nix
     ./services.nix
     ./system.nix
-    ./tui.nix
     ./users.nix
   ];
 }
