18f4c38dec7f1ece3c94e4805ec2c61cedd7a6be
luravoid <example@example.com>
2025-01-12 23:57:55 +0100

    edit laptop host

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -6,6 +6,9 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/nixos
+
+    ../../modules/profiles/laptop
+    ../../modules/profiles/graphical
   ];
 
   environment.systemPackages = with pkgs; [
@@ -53,5 +56,77 @@
         cpupower
       ]
       ++ [pkgs.cpupower-gui];
+  };
+
+  garden = {
+    device = {
+      type = "desktop";
+      cpu = "amd";
+      gpu = "amd";
+      # hasTPM = "true"; # TODO
+      hasBluetooth = true;
+      hasSound = true;
+      keyboard = "pl";
+    };
+
+    environment = {
+      desktop = "Hyprland";
+    };
+
+    system = {
+      mainUser = "scay";
+
+      boot = {
+        loader = "systemd-boot";
+      };
+
+      video.enable = true;
+      sound.enable = true;
+      bluetooth.enable = true;
+      virtualization.enable = false;
+      printing.enable = false;
+
+      loginManager = {
+        name = "greetd";
+        autoLogin.enable = true;
+      };
+    };
+
+    programs = {
+      cli = {
+        enable = true;
+        shell = "zsh";
+        git.enable = true;
+        starship.enable = true;
+      };
+
+      tui = {
+        enable = true;
+        nvim.enable = true;
+        yazi.enable = true;
+        fzf.enable = true;
+        btop.enable = true;
+        lazygit.enable = true;
+      };
+
+      gui = {
+        enable = true;
+        thunar.enable = true;
+        image = {
+          vimiv.enable = true;
+        };
+        terminal = {
+          kitty.enable = true;
+        };
+      };
+
+      gaming = {
+        enable = false;
+      };
+    };
+
+    services = {
+      syncthing.enable = false;
+    };
   };
 }
