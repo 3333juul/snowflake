c8533bff5e01eb71b93b7cd97baa7f4c742d78fa
luravoid <example@example.com>
2025-01-14 23:18:34 +0100

    edit shell.nix

--- modules/base/shell.nix
+++ modules/base/shell.nix
@@ -3,11 +3,13 @@
   config,
   ...
 }: let
+  inherit (lib.modules) mkIf;
+
   cfg = config.garden.programs;
 in {
   programs = {
     # home-manager needs it declared also here
-    zsh.enable = lib.mkIf (cfg.cli.shell == "zsh") true;
-    fish.enable = lib.mkIf (cfg.cli.shell == "fish") true;
+    zsh.enable = mkIf (cfg.cli.shell == "zsh") true;
+    fish.enable = mkIf (cfg.cli.shell == "fish") true;
   };
 }
