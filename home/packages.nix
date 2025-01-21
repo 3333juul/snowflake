8dd2f880f4994e252b1ebe8d50a5c926b27a3e53
luravoid <example@example.com>
2025-01-21 19:50:40 +0100

    edit home packages

--- home/packages.nix
+++ home/packages.nix
@@ -4,17 +4,9 @@
   ...
 }: {
   home.packages = with pkgs; [
-    # Python
-    (python3.withPackages (
-      ps:
-        with ps; [
-          pytesseract
-          pyqt5
-          pynput
-          numpy
-        ]
-    ))
-
+    # python
+    python3
+    # nix code formatter
     inputs.alejandra.defaultPackage.${system}
   ];
 }
