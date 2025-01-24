7b69fd8c0dbd4d95987e4a892213f418827fdacb
luravoid <example@example.com>
2025-01-24 19:57:23 +0100

    add lib folder

---
+++ lib/mkHostConfig.nix
@@ -0,0 +1,41 @@
+{
+  inputs,
+  lib,
+}: let
+  classToOS = class:
+    if class == "nixos"
+    then "linux"
+    else "darwin";
+
+  mkHostConfig = {
+    host,
+    class ? "nixos", # default class: nixos
+    system ? "x86_64", # default system: x86_64-linux
+    profiles ? [],
+  }: let
+    fullSystem = "${system}-${classToOS class}";
+  in
+    lib.nixosSystem {
+      specialArgs = {
+        inherit inputs;
+      };
+
+      modules =
+        [
+          # common modules between all systems
+          ./modules/base
+          # modules per class: nixos, darwin
+          ./modules/${class}
+          # modules per host
+          ./hosts/${host}
+
+          # set hostname
+          {networking.hostName = host;}
+          # set system
+          {nixpkgs.hostPlatform = fullSystem;}
+        ]
+        ++ profiles;
+    };
+in {
+  inherit mkHostConfig;
+}
