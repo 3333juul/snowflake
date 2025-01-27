d8520adc2ebdab7361553037045b3a7f77a9fc5e
luravoid <example@example.com>
2025-01-27 21:46:14 +0100

    edit nix.nix

--- modules/base/nix/nix.nix
+++ modules/base/nix/nix.nix
@@ -1,13 +1,22 @@
 {
   nix = {
     settings = {
+      # automatically optimise symlinks
       auto-optimise-store = true;
+
+      # don't warn me if the current working tree is dirty
+      warn-dirty = false;
+
+      # continue building derivations even if one fails
+      keep-going = true;
+
+      # show more log lines for failed builds
+      log-lines = 30;
+
       experimental-features = [
         "nix-command"
         "flakes"
       ];
-      # don't warn me if the current working tree is dirty
-      warn-dirty = false;
     };
   };
 }
