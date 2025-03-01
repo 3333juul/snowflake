ca5184065199e3461a7714de02aed8a7a003a603
luravoid <example@example.com>
2025-03-01 22:02:16 +0100

    restic: automatic creation of systemd services for all the backups

--- modules/nixos/services/sync/restic.nix
+++ modules/nixos/services/sync/restic.nix
@@ -4,14 +4,41 @@
   lib,
   ...
 }: let
-  inherit (lib.modules) mkIf;
-  inherit (lib.attrsets) mapAttrs;
+  inherit (lib.modules) mkIf mkMerge;
+  inherit (lib.attrsets) mapAttrs mapAttrs' nameValuePair;
   inherit (lib.secrets) mkSecret;
   inherit (config.garden.system) mainUser;
   inherit (config.garden.services) restic;
+  inherit (config.garden) device;
   inherit (builtins) elem;
 
   user = config.users.users.${mainUser};
+
+  # Create a notification service for a given backup
+  mkNotifyService = backupName:
+    nameValuePair "notify-backup-failed-${backupName}" {
+      enable = true;
+      description = "Notify on failed backup for ${backupName}";
+      serviceConfig = {
+        Type = "oneshot";
+        User = user.name;
+      };
+
+      # required for notify-send to work
+      environment.DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
+
+      script = ''
+        ${pkgs.libnotify}/bin/notify-send --urgency=critical \
+          "Backup failed for ${backupName}" \
+          "$(journalctl -u restic-backups-${backupName} -n 5 -o cat)"
+      '';
+    };
+
+  # Link a backup service to its notification service
+  mkOnFailureLink = backupName:
+    nameValuePair "restic-backups-${backupName}" {
+      unitConfig.OnFailure = "notify-backup-failed-${backupName}.service";
+    };
 
   # only enable these backups that are defined in garden.services.restic.backups
   enabledBackups = mapAttrs (name: value: mkIf (elem name restic.backups) value);
@@ -28,7 +55,6 @@ in {
         repository = "rclone:onedrive:backups/${config.networking.hostName}";
         passwordFile = config.age.secrets.restic-password.path;
         rcloneConfigFile = config.age.secrets.rclone.path;
-        user = "doesntexist";
 
         paths = [
           "${user.home}/media"
@@ -42,30 +68,10 @@ in {
       };
     };
 
-    # Notify when backup fails
-    systemd.services."notify-backup-failed" = {
-      enable = true;
-      description = "Notify on failed backup";
-      serviceConfig = {
-        Type = "oneshot";
-        User = user.name;
-      };
-
-      # Required for notify-send to work
-      environment.DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
-
-      # doesn't change anything
-      # environment.DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/${
-      #   toString user.uid
-      # }/bus";
-
-      script = ''
-        ${pkgs.libnotify}/bin/notify-send --urgency=critical \
-          "Backup failed" \
-          "$(journalctl -u restic-backups-onedrive -n 5 -o cat)"
-      '';
-    };
-
-    systemd.services.restic-backups-onedrive.unitConfig.OnFailure = "notify-backup-failed.service";
+    # Generate all systemd services
+    systemd.services = mkIf (device.type != "server") (mkMerge [
+      (mapAttrs' (name: _: mkNotifyService name) config.services.restic.backups)
+      (mapAttrs' (name: _: mkOnFailureLink name) config.services.restic.backups)
+    ]);
   };
 }
