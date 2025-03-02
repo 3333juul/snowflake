ee1c0dd0d4f8128b3ef1c93494759c8af55cf9ff
luravoid <example@example.com>
2025-03-02 23:14:34 +0100

    discord: add comments

--- home/gui/discord/default.nix
+++ home/gui/discord/default.nix
@@ -6,15 +6,26 @@
 }: let
   inherit (lib.modules) mkIf;
   cfg = osConfig.garden.programs;
+  # userSettings = {
+  #   plugins = {
+  #     BetterNotesBox.enabled = false;
+  #     ShowHiddenChannels.enabled = false;
+  #     YoutubeAdblock.enabled = true;
+  #     BetterGifPicker.enabled = true;
+  #     SpotifyControls.enabled = false;
+  #   };
+  # };
 in {
   config = mkIf cfg.discord.enable {
     home.packages = with pkgs; [
-      # discord
       (discord.override {
         withVencord = true;
       })
       vesktop
     ];
+
     xdg.configFile."Vencord/themes/gruvbox.theme.css".source = ./gruvbox.css;
+    # xdg.configFile."Vencord/settings/settings.json".text = builtins.toJSON userSettings;
   };
 }
+
