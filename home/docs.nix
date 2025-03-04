6f379cc21ba5f202e6ff7df6655526cd9de1c1bd
luravoid <example@example.com>
2025-03-04 18:19:42 +0100

    edit quicklinks

--- home/docs.nix
+++ home/docs.nix
@@ -3,9 +3,7 @@
   inherit (lib.modules) mkForce;
 in {
   # the docs create issues that are really hard to debug
-  # such as ones where lib does not get passed correctly,
-  # see an example of me complaining and then finding this as the fix
-  # https://akko.isabelroses.com/notice/AkdGj5udRhYIKqTeng
+  # such as ones where lib does not get passed correctly
   manual = mapAttrs (_: mkForce) {
     html.enable = false;
     json.enable = false;
