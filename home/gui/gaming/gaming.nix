7da76063654803162ea0d620a5e7e97f4772bf5e
luravoid <example@example.com>
2025-03-12 09:32:30 +0100

    edit gaming.nix

--- home/gui/gaming/gaming.nix
+++ home/gui/gaming/gaming.nix
@@ -5,15 +5,18 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
+  inherit (lib.lists) optionals;
 
   cfg = osConfig.garden.programs.gaming;
 in {
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      (mkIf cfg.lutris.enable lutris)
-      gamescope
-      mangohud
-      winetricks
-    ];
+    home.packages = with pkgs;
+      [
+        gamescope
+        mangohud
+        winetricks
+      ]
+      ++ optionals cfg.lutris.enable
+      [lutris];
   };
 }
