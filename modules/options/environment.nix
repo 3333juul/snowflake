8d18b4bf86b98574ae53f6e16f4263d92d9d93b3
luravoid <example@example.com>
2025-03-14 16:27:36 +0100

    small fix

--- modules/options/environment.nix
+++ modules/options/environment.nix
@@ -75,16 +75,16 @@ in {
       default = "/${ldTernary pkgs "home" "Users"}/${sys.mainUser}/snowflake";
       description = "The path to the configuration";
     };
-
-    config.assertions = [
-      {
-        assertion = sys.useHomeManager -> sys.mainUser != null;
-        message = "system.mainUser must be set while useHomeManager is enabled";
-      }
-      {
-        assertion = env.flakePath != null -> sys.mainUser != null;
-        message = "system.mainUser must be set if a flakePath is specified";
-      }
-    ];
   };
+
+  config.assertions = [
+    {
+      assertion = sys.useHomeManager -> sys.mainUser != null;
+      message = "system.mainUser must be set while useHomeManager is enabled";
+    }
+    {
+      assertion = env.flakePath != null -> sys.mainUser != null;
+      message = "system.mainUser must be set if a flakePath is specified";
+    }
+  ];
 }
