cd82106fb974302fb576ebbb6b8bc4f6cd243953
luravoid <example@example.com>
2025-03-18 23:00:28 +0100

    nvf: edit langs

--- home/tui/nvf/plugins/lang/languages.nix
+++ home/tui/nvf/plugins/lang/languages.nix
@@ -1,3 +1,7 @@
+# {osConfig, ...}: let
+#   inherit (osConfig.garden.environment) flakePath;
+#   inherit (osConfig.networking) hostName;
+# in {
 {
   programs.nvf.settings.vim = {
     languages = {
@@ -8,13 +12,30 @@
 
       bash.enable = true;
       lua.enable = true;
-      nix.enable = true;
       python.enable = true;
+
+      nix = {
+        enable = true;
+        # TODO:
+        # lsp = {
+        #   server = "nixd";
+        #   options = {
+        #     nixos = {
+        #       expr = "(builtins.getFlake ${flakePath}).nixosConfigurations.${hostName}.options";
+        #     };
+        #   };
+        # };
+      };
+
       rust = {
         enable = false;
         crates.enable = false;
       };
-      markdown.enable = true;
+
+      markdown = {
+        enable = true;
+        format.type = "prettierd";
+      };
     };
   };
 }
