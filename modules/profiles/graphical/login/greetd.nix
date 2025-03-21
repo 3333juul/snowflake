f725bf677a83184516c5b0b82ebca0967801a64d
luravoid <example@example.com>
2025-03-21 03:38:21 +0100

    edit greetd

--- modules/profiles/graphical/login/greetd.nix
+++ modules/profiles/graphical/login/greetd.nix
@@ -6,16 +6,32 @@
 }: let
   inherit (lib.modules) mkIf;
   inherit (config.garden.system) mainUser;
+  inherit (lib.strings) concatStringsSep;
 
   cfg = config.garden.system.loginManager;
+
+  sessionData = config.services.displayManager.sessionData.desktops;
+  sessionPath = concatStringsSep ":" [
+    "${sessionData}/share/xsessions"
+    "${sessionData}/share/wayland-sessions"
+  ];
 in {
   services.greetd = mkIf (cfg.name == "greetd") {
     enable = true;
     restart = !cfg.autoLogin.enable;
+
     settings = {
       default_session = {
         user = "greeter";
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --time-format '%F %R'";
+        command = concatStringsSep " " [
+          "${pkgs.greetd.tuigreet}/bin/tuigreet"
+          "--time"
+          "--time-format '%F %R'"
+          "--remember"
+          "--remember-user-session"
+          "--asterisks"
+          "--sessions '${sessionPath}'"
+        ];
       };
 
       initial_session = mkIf cfg.autoLogin.enable {
