d559b7e35fce679d781d6566654ee392f91255bc
luravoid <example@example.com>
2025-03-30 09:55:34 +0200

    vscode: fix deprecated code

--- home/packages/gui/vscodium.nix
+++ home/packages/gui/vscodium.nix
@@ -20,84 +20,89 @@ in {
   programs.vscode = mkIf cfg.vscodium.enable {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      # nix language
-      bbenoist.nix
-      # nix-shell suport
-      arrterian.nix-env-selector
-      # python
-      # ms-python.python # broken
-      # C/C++
-      ms-vscode.cpptools
-      # OCaml
-      ocamllabs.ocaml-platform
-      # Color theme
-      jdinhlife.gruvbox
-      # sainnhe.gruvbox-material
-      jonathanharty.gruvbox-material-icon-theme
-      # nvim
-      asvetliakov.vscode-neovim
-      # alejandra
-      kamadorueda.alejandra
-    ];
-    userSettings = {
-      "update.mode" = "none";
-      "extensions.autoUpdate" = false; # This stuff fixes vscode freaking out when theres an update
-      "window.titleBarStyle" = "custom"; # needed otherwise vscode crashes, see https://github.com/NixOS/nixpkgs/issues/246509
-      "keyboard.dispatch" = "keyCode"; # needed for respecting caps:escape swap
 
-      "window.menuBarVisibility" = "toggle";
-      "editor.fontFamily" = "'FiraCode Nerd Font', 'SymbolsNerdFont', 'monospace', monospace";
-      "terminal.integrated.fontFamily" = "'FiraCode Nerd Font', 'SymbolsNerdFont'";
-      "editor.fontSize" = 16;
-      "workbench.colorTheme" = "Gruvbox Dark Hard";
-      "workbench.iconTheme" = "gruvbox-material-icon-theme";
-      "material-icon-theme.folders.theme" = "classic";
-      "vsicons.dontShowNewVersionMessage" = true;
-      "explorer.confirmDragAndDrop" = false;
-      "editor.fontLigatures" = true;
-      "editor.minimap.enabled" = true;
-      "workbench.startupEditor" = "none";
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        # nix language
+        bbenoist.nix
+        # nix-shell suport
+        arrterian.nix-env-selector
+        # python
+        # ms-python.python # broken
+        # C/C++
+        ms-vscode.cpptools
+        # OCaml
+        ocamllabs.ocaml-platform
+        # Color theme
+        jdinhlife.gruvbox
+        # sainnhe.gruvbox-material
+        jonathanharty.gruvbox-material-icon-theme
+        # nvim
+        asvetliakov.vscode-neovim
+        # alejandra
+        kamadorueda.alejandra
+      ];
 
-      "editor.formatOnSave" = true;
-      "editor.formatOnType" = true;
-      "editor.formatOnPaste" = true;
+      userSettings = {
+        "update.mode" = "none";
+        "extensions.autoUpdate" = false; # This stuff fixes vscode freaking out when theres an update
+        "window.titleBarStyle" = "custom"; # needed otherwise vscode crashes, see https://github.com/NixOS/nixpkgs/issues/246509
+        "keyboard.dispatch" = "keyCode"; # needed for respecting caps:escape swap
 
-      "workbench.layoutControl.type" = "menu";
-      "workbench.editor.limit.enabled" = true;
-      "workbench.editor.limit.value" = 10;
-      "workbench.editor.limit.perEditorGroup" = true;
-      "workbench.editor.showTabs" = "multiple";
-      #"files.autoSave" = "onWindowChange";
-      "explorer.openEditors.visible" = 0;
-      "breadcrumbs.enabled" = false;
-      "editor.renderControlCharacters" = false;
-      #"workbench.activityBar.location" = "hidden";
-      "workbench.statusBar.visible" = true;
-      "editor.scrollbar.verticalScrollbarSize" = 5;
-      "editor.scrollbar.horizontalScrollbarSize" = 5;
-      #"editor.scrollbar.vertical" = "hidden";
-      #"editor.scrollbar.horizontal" = "hidden";
-      "workbench.layoutControl.enabled" = false;
+        "window.menuBarVisibility" = "toggle";
+        "editor.fontFamily" = "'FiraCode Nerd Font', 'SymbolsNerdFont', 'monospace', monospace";
+        "terminal.integrated.fontFamily" = "'FiraCode Nerd Font', 'SymbolsNerdFont'";
+        "editor.fontSize" = 16;
+        "workbench.colorTheme" = "Gruvbox Dark Hard";
+        "workbench.iconTheme" = "gruvbox-material-icon-theme";
+        "material-icon-theme.folders.theme" = "classic";
+        "vsicons.dontShowNewVersionMessage" = true;
+        "explorer.confirmDragAndDrop" = false;
+        "editor.fontLigatures" = true;
+        "editor.minimap.enabled" = true;
+        "workbench.startupEditor" = "none";
 
-      "editor.mouseWheelZoom" = true;
+        "editor.formatOnSave" = true;
+        "editor.formatOnType" = true;
+        "editor.formatOnPaste" = true;
 
-      # needed for nvim
-      "extensions.experimental.affinity" = {
-        "asvetliakov.vscode-neovim" = 1;
+        "workbench.layoutControl.type" = "menu";
+        "workbench.editor.limit.enabled" = true;
+        "workbench.editor.limit.value" = 10;
+        "workbench.editor.limit.perEditorGroup" = true;
+        "workbench.editor.showTabs" = "multiple";
+        #"files.autoSave" = "onWindowChange";
+        "explorer.openEditors.visible" = 0;
+        "breadcrumbs.enabled" = false;
+        "editor.renderControlCharacters" = false;
+        #"workbench.activityBar.location" = "hidden";
+        "workbench.statusBar.visible" = true;
+        "editor.scrollbar.verticalScrollbarSize" = 5;
+        "editor.scrollbar.horizontalScrollbarSize" = 5;
+        #"editor.scrollbar.vertical" = "hidden";
+        #"editor.scrollbar.horizontal" = "hidden";
+        "workbench.layoutControl.enabled" = false;
+
+        "editor.mouseWheelZoom" = true;
+
+        # needed for nvim
+        "extensions.experimental.affinity" = {
+          "asvetliakov.vscode-neovim" = 1;
+        };
       };
+
+      # Keybindings
+      keybindings = [
+        {
+          key = "ctrl+q";
+          command = "editor.action.commentLine";
+          when = "editorTextFocus && !editorReadonly";
+        }
+        {
+          key = "ctrl+s";
+          command = "workbench.action.files.saveFiles";
+        }
+      ];
     };
-    # Keybindings
-    keybindings = [
-      {
-        key = "ctrl+q";
-        command = "editor.action.commentLine";
-        when = "editorTextFocus && !editorReadonly";
-      }
-      {
-        key = "ctrl+s";
-        command = "workbench.action.files.saveFiles";
-      }
-    ];
   };
 }
