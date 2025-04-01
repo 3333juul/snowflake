fecd9e29bcbd366ff509215ba51eafea26cdfee3
luravoid <example@example.com>
2025-04-01 03:20:39 +0200

    remove unnecessary variables

--- modules/nixos/profiles/graphical/display/wayland/environment.nix
+++ modules/nixos/profiles/graphical/display/wayland/environment.nix
@@ -10,20 +10,19 @@ in {
   # TODO: Change to isWayland, remove variables not associated with wayland
   environment.variables = mkIf (cfg == "Hyprland") {
     NIXOS_OZONE_WL = "1";
-    __GL_GSYNC_ALLOWED = "0";
-    __GL_VRR_ALLOWED = "0";
     _JAVA_AWT_WM_NONEREPARENTING = "1";
-    SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
-    GDK_BACKEND = "wayland";
     ANKI_WAYLAND = "1";
-    DIRENV_LOG_FORMAT = "";
-    WLR_DRM_NO_ATOMIC = "1";
     MOZ_ENABLE_WAYLAND = "1";
-    WLR_BACKEND = "vulkan";
-    WLR_RENDERER = "vulkan";
-    WLR_NO_HARDWARE_CURSORS = "1";
+
+    GDK_BACKEND = "wayland,x11";
     XDG_SESSION_TYPE = "wayland";
     SDL_VIDEODRIVER = "wayland";
     CLUTTER_BACKEND = "wayland";
+
+    # __GL_GSYNC_ALLOWED = "0";
+    # __GL_VRR_ALLOWED = "0";
+    # WLR_DRM_NO_ATOMIC = "1";
+    # WLR_BACKEND = "vulkan";
+    # WLR_RENDERER = "vulkan";
   };
 }
