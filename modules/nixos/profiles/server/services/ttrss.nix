d55c59e69fa2e27e39d945764c28f3f94541ec1c
luravoid <example@example.com>
2025-04-05 21:58:20 +0200

    edit ttrss

--- modules/nixos/profiles/server/services/ttrss.nix
+++ modules/nixos/profiles/server/services/ttrss.nix
@@ -1,20 +1,35 @@
-{
-  # services.tt-rss = {
-  #   enable = false;
-  #   selfUrlPath = "http://localhost";
+{pkgs, ...}: let
+  # fix theme: https://github.com/levito/tt-rss-feedly-theme/issues/122
+  themeFeedly = pkgs.tt-rss-theme-feedly.overrideAttrs {
+    src = pkgs.fetchFromGitHub {
+      owner = "levito";
+      repo = "tt-rss-feedly-theme";
+      rev = "dist";
+      sha256 = "sha256-0U2APDh++U7qWwv9ky9nEZ8WvsSbWBTSUwqQqkIkaqU=";
+    };
+  };
+in {
+  services.tt-rss = {
+    enable = true;
+    selfUrlPath = "http://localhost";
+    virtualHost = "tt-rss";
+    themePackages = [themeFeedly];
 
-  #themePackages = [pkgs.tt-rss-theme-feedly];
-  #pluginPackages = [pkgs.tt-rss-plugin-auth-ldap];
-  # plugins = [
-  #   "auth_internal"
-  #   "auth_ldap"
-  #   "note"
-  #   "updater"
-  #   "api_feedreader"
-  # ];
+    database = {
+      type = "pgsql";
+    };
+  };
 
-  #   database = {
-  #     type = "pgsql";
-  #   };
-  # };
+  services.nginx = {
+    enable = true;
+
+    virtualHosts."tt-rss" = {
+      listen = [
+        {
+          port = 4124;
+          addr = "0.0.0.0";
+        }
+      ];
+    };
+  };
 }
