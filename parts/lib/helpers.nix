e5b6f64f211819daf1b406c16c06039c10f1a995
luravoid <example@example.com>
2025-04-09 19:56:02 +0200

    lib.helpers.addBackupPaths: add description

--- parts/lib/helpers.nix
+++ parts/lib/helpers.nix
@@ -32,6 +32,36 @@
   */
   filterEnabled = keys: attrs: filterAttrs (name: _: builtins.elem name keys) attrs;
 
+  /*
+  *
+  convenient helper for adding paths to multiple backups.
+
+  # Arguments
+
+  - [config] the configuration that nixosConfigurations provides
+  - [backups]: list of backups to select, or `["*"]` to select all enabled backups
+  - [paths]: list of paths to add to each selected backup
+
+  # Type
+
+  ```
+  addBackupPaths :: AttrSet -> [String] -> [Path] -> AttrSet
+  ```
+
+  # Example
+
+  ```nix
+  services.restic.backups = addBackupPaths config ["backup1" "backup2"] [
+    "/path/1"
+    "/path/2"
+  ]
+  => {
+    backup1 = { paths = ["/path/1" "/path/2"]; };
+    backup2 = { paths = ["/path/1" "/path/2"]; };
+  };
+  ```
+  */
+
   addBackupPaths = config: backups: paths: let
     names =
       if builtins.elem "*" backups
