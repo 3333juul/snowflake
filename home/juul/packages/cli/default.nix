c957f7e8fe167f32583d30afb07a32f8f3ed1326
luravoid <example@example.com>
2025-07-30 14:55:16 +0200

    add gallery-dl

--- home/juul/packages/cli/default.nix
+++ home/juul/packages/cli/default.nix
@@ -3,6 +3,7 @@
     ./shell
     ./aichat.nix
     ./bat.nix
+    ./downloaders.nix
     ./fastfetch.nix
     ./git.nix
     ./shared.nix
---
+++ home/juul/packages/cli/downloaders.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  osConfig,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+
+  cfg = osConfig.garden.presets.cli;
+in {
+  config = mkIf cfg.enable {
+    programs.gallery-dl = {
+      enable = true;
+    };
+
+    # home.packages = [
+    #   pkgs.instaloader
+    # ];
+  };
+}
